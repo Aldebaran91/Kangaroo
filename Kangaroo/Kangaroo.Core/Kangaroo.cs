a375140a2d956e539fe3b2d8bdf7c1340eb13345
gergo.laszlo.kiss <gergo.laszlo.kiss@gmail.com>
2018-03-27 03:09:13 +0200

    Added comments to Kagaroo class.

--- Kangaroo/Kangaroo.Core/Kangaroo.cs
+++ Kangaroo/Kangaroo.Core/Kangaroo.cs
@@ -5,38 +5,74 @@ using System.Threading.Tasks;
 
 namespace Kangaroo
 {
+		/// <summary>
+		/// Primary class of the Kangaroo library.
+		/// </summary>
     public sealed class Kangaroo
     {
+				/// <summary>
+				/// Enumerable property with a collection of data objects to be exported.
+				/// </summary>
         public IEnumerable<object> LoggingData { get; set; }
-        public IEnumerable<IExportManager<object, object>> ExportHandler { get; set; }
-        public IExportSettings ExportSettings { get; set; }
+				/// <summary>
+				/// Enumerable property with a collection of specific/custom export handlers to be used.
+				/// </summary>
+				public IEnumerable<IExportManager<object, object>> ExportHandler { get; set; }
+				/// <summary>
+				/// Property for export settings as defined by specific/custom implementation.
+				/// </summary>
+				public IExportSettings ExportSettings { get; set; }
 
-        public void AddData(object data)
+				/// <summary>
+				/// Method for adding data to the collection of data objects to be exported. 
+				/// </summary>
+				/// <param name="data"></param>
+				public void AddData(object data)
         {
             throw new NotImplementedException();
         }
 
-        public Task AddDataAsync(object data)
+				/// <summary>
+				/// Method for adding data asynchronously to the collection of data objects to be exported. 
+				/// </summary>
+				/// <param name="data"></param>
+				/// <returns></returns>
+				public Task AddDataAsync(object data)
         {
             throw new NotImplementedException();
         }
 
-        public void AddData(object data, string category)
+				/// <summary>
+				/// Overloaded method for addig data to the collection of data objects to be exported, and also passing the category for the data to be assigned to.
+				/// </summary>
+				/// <param name="data"></param>
+				public void AddData(object data, string category)
         {
             throw new NotImplementedException();
         }
 
-        public Task AddDataAsync(object data, string category)
+				/// <summary>
+				/// Overloaded method for addig data asynchronously to the collection of data objects to be exported, and also passing the category for the data to be assigned to.
+				/// </summary>
+				/// <param name="data"></param>
+				public Task AddDataAsync(object data, string category)
         {
             throw new NotImplementedException();
         }
 
-        public void StartExport()
+				/// <summary>
+				/// Method for staring the export.
+				/// </summary>
+				public void StartExport()
         {
             throw new NotImplementedException();
         }
 
-        public Task StartExportAsync()
+				/// <summary>
+				/// Method for asynchronously staring the export. Ansynchronous export is realized by returning a value of type Task.
+				/// </summary>
+				/// <returns></returns>
+				public Task StartExportAsync()
         {
             throw new NotImplementedException();
         }
