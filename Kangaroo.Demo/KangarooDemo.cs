2ac6618ba7b6ce885f89aa95101d8c2a07c271e2
gergo.laszlo.kiss <gergo.laszlo.kiss@gmail.com>
2018-04-18 22:15:04 +0200

    Created/updated Kangaroo.Demo project

---
+++ Kangaroo.Demo/KangarooDemo.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kangaroo;
+using Kangaroo.Core;
+
+
+namespace Kangaroo.Demo
+{
+	class KangarooDemo
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Starting Kangaroo Demo!");
+
+			KangarooStore<Exception> kangaroo = new KangarooStore<Exception>();
+
+			KangarooExporter<Exception, string> exporter = new KangarooExceptionExporter();
+
+			exporter.Converter = new KangarooConvertExcpetionToString();
+
+			exporter.Filter = (x) =>
+			{
+				return x is NullReferenceException;
+			};
+
+			exporter.Worker = new KangarooExportWorkerStringToConsole();
+
+			//kangaroo.ExportHandler.Add(exporter);
+			//TODO discuss
+			kangaroo.AddExporter(exporter);
+			Console.WriteLine("Throwing test exception!");
+			try
+			{
+				throw new System.ArgumentException("Test exception message.");
+			}
+			catch (Exception ex)
+			{
+				kangaroo.AddData(ex);
+			}
+			finally
+			{
+				Console.WriteLine("Stopping Kangaroo Demo!");
+			}
+
+		}
+
+
+		public class KangarooConvertExcpetionToString : IKangarooConverter<Exception, string>
+		{
+			public string Convert(Exception data)
+			{
+				return data.ToString();
+			}
+		}
+
+		public class KangarooExceptionExporter : KangarooExporter<Exception, string>
+		{
+			public IKangarooConverter<Exception, string> Converter { get; set; }
+
+			public IKangarooExportWorker<string> Worker { get; set; }
+		}
+
+		public class KangarooExportWorkerStringToConsole : IKangarooExportWorker<string>
+		{
+			public void Export(IEnumerable<string> input)
+			{
+				foreach (var item in input)
+					System.Console.WriteLine($"Kangaroo>> {item}");
+			}
+		}
+
+
+	}
+}
