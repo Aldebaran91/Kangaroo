5a0f99e0fa840a70f51168903bfc1f970c18d0d4
gergo.laszlo.kiss <gergo.laszlo.kiss@gmail.com>
2018-04-18 22:57:33 +0200

    Updated unit tests

--- Kangaroo/Kangaroo.Core.Test/Implementations/KangarooConverterUnitTests.cs
+++ Kangaroo/Kangaroo.Core.Test/Implementations/KangarooConverterUnitTests.cs
@@ -1,13 +1,29 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace Kangaroo.Core.Test.Implementations
 {
+
     [TestClass]
     public class KangarooConverterUnitTests
     {
+
         [TestMethod]
-        public void TestMethod1()
+        public void KangarooConverterTestMethod1()
         {
-        }
-    }
+			var obj = new KangarooConvertExcpetionToString();
+			var result = obj.Convert(new Exception());
+			Assert.IsInstanceOfType(result,typeof(string));
+		}
+
+		public class KangarooConvertExcpetionToString : IKangarooConverter<Exception, string>
+		{
+			public string Convert(Exception data)
+			{
+				return data.ToString();
+			}
+		}
+
+
+	}
 }
---
+++ Kangaroo/Kangaroo.Core.Test/Implementations/KangarooExportWorkerUnitTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kangaroo.Core.Test.Implementations
+{
+	[TestClass]
+	public class KangarooExportWorkerUnitTests
+	{
+		[TestMethod]
+		public void KangarooExportWorkerTestMethod1()
+		{
+			StringWriter sw = new StringWriter();
+			Console.SetOut(sw);
+			var obj = new KangarooExportWorkerStringToConsole();
+			List<string> test = new List<string>{ "a", "b", "c" };
+			obj.Export(test);
+			Assert.AreEqual<string>("abc", sw.ToString());
+			sw.Close();
+
+		}
+
+		public class KangarooExportWorkerStringToConsole : IKangarooExportWorker<string>
+		{
+			public void Export(IEnumerable<string> input)
+			{
+				foreach (var item in input)
+					System.Console.Write(item);
+			}
+		}
+	}
+}
